b49f4c632a2bac7df89fa99276a86d29214052d8
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-11-11 11:05:55 +0100

    posts comments showed

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 
 function Post(props) {
+  let comments = props.item.comments;
+
   return (
     <Card style={{ width: "80%", maxWidth: "32rem", margin: "20px" }}>
       <Card.Body>
@@ -10,6 +12,15 @@ function Post(props) {
           Author: {props.item.user}
         </Card.Subtitle>
         <Card.Text>{props.item.content}</Card.Text>
+        <Card.Text>Comments:</Card.Text>
+
+        {comments.map((comment, index) => {
+          return (
+            <Card.Text comment={comment} key={index}>
+              {comment}
+            </Card.Text>
+          );
+        })}
         <Button variant="primary" className="float-end">
           Comment
         </Button>
